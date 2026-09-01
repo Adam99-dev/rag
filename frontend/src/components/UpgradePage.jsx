dd1b5ab8d0cce5831916de21c9f6dbbc66c14890
Syed Adam Ali <kingsilverali@gmail.com>
2026-09-01 23:56:32 +0530

    payment gateway fixed

--- frontend/src/components/UpgradePage.jsx
+++ frontend/src/components/UpgradePage.jsx
@@ -472,6 +472,8 @@ const UpgradePage = ({ onBack, onSuccess, user }) => {
     setPhase("idle");
     land();
     setVerdict({ say: "", sub: "" });
+    setErrors({ name: "", number: "", expiry: "", cvc: "" });
+    setComplete({ number: false, expiry: false, cvc: false });
     requestAnimationFrame(() => numElRef.current?.focus?.());
   };
 
